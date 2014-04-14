46512f8b7778861e49543e4c2eb6081d5cd0c74c
luobao <sgyzlb@126.com>
2014-04-14 15:59:24 +0800

    first version

---
+++ CocoaPodsDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CocoaPodsDemo
+//
+//  Created by luobao on 14-4-13.
+//  Copyright (c) 2014年 luobao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CocoaPodsDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CocoaPodsDemo
+//
+//  Created by luobao on 14-4-13.
+//  Copyright (c) 2014年 luobao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AFNetworking.h>
+@interface ViewController : UIViewController
+
+@end
